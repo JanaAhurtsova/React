85a0bd5f45ab2c289f4bb99aedef6dd85114cfc6
Jana Ahurtsova <byjankaaa@gmail.com>
2023-03-27 01:33:58 +0300

    fix: fix tests

--- components/src/components/form/form.test.tsx
+++ components/src/components/form/form.test.tsx
@@ -2,17 +2,18 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Form from './form';
 
 describe('Form component', () => {
-  beforeEach(() => {
+  it('render component', () => {
     const mockFn = vi.fn();
     render(<Form addCard={mockFn} />);
-  });
 
-  it('render component', () => {
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     expect(screen.getByRole('combobox')).toBeInTheDocument();
   });
 
   it('all fields are empty', () => {
+    const mockFn = vi.fn();
+    render(<Form addCard={mockFn} />);
+
     fireEvent.click(screen.getByRole('button', { name: /Submit/i }));
 
     expect(screen.getByText(/name cannot be blank/i)).toBeInTheDocument();
@@ -24,15 +25,23 @@ describe('Form component', () => {
   });
 
   it('reset form', () => {
+    const mockFn = vi.fn();
+    render(<Form addCard={mockFn} />);
+
     fireEvent.change(screen.getByRole('textbox'), { target: { value: 'Test' } });
+    fireEvent.click(screen.getAllByRole('radio')[1]);
 
     fireEvent.click(screen.getByRole('button', { name: /reset/i }));
 
     expect(screen.getByRole('textbox')).not.toHaveAttribute('value');
+    expect(screen.getAllByRole('radio')[1]).not.toHaveAttribute('checked');
   });
 
   it('invalid date', () => {
-    fireEvent.change(document.querySelector('[type="date"]')!, {
+    const mockFn = vi.fn();
+    const { container } = render(<Form addCard={mockFn} />);
+
+    fireEvent.change(container.querySelector('[type="date"]')!, {
       target: { value: '2024-03-27' },
     });
 
@@ -42,10 +51,12 @@ describe('Form component', () => {
   });
 
   it('invalid file format upload', () => {
+    const mockFn = vi.fn();
+    const { container } = render(<Form addCard={mockFn} />);
     const mockPdf = new File(['test'], 'test.pdf', { type: 'application/pdf' });
     window.URL.createObjectURL = vi.fn();
 
-    fireEvent.change(document.querySelector('[type="file"]')!, {
+    fireEvent.change(container.querySelector('[type="file"]')!, {
       target: { files: [mockPdf] },
     });
 
